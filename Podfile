5238af9e187354d87e56f229b204defa66912cc4
Sauvik Dolui <sauvik.dolui@innofied.com>
2017-07-31 17:44:17 +0530

    Inital Push

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'QuickBloxExploration' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for QuickBloxExploration
+pod 'QuickBlox'
+
+end
--- QuickBloxExploration/AppDelegate.swift
+++ QuickBloxExploration/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Quickblox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        setUpQuickBlox()
+        connectToQuickBlox()
+        
+        
         return true
     }
 
@@ -42,5 +48,79 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+
+// MARK: - CUSTOM SETTINGS
+extension AppDelegate {
+    func setUpQuickBlox() {
+        
+        QBSettings.setApplicationID(60892)
+        QBSettings.setAuthKey("eGEzeQfF-7vZRqw")
+        QBSettings.setAccountKey("tGBCuP5LWQGjuoPagEWz")
+        QBSettings.setAuthSecret("VFn7uA4DRzqYzFJ")
+        
+        // enabling carbons for chat
+        QBSettings.setCarbonsEnabled(true)
+        
+        // Enables Quickblox REST API calls debug console output.
+        QBSettings.setLogLevel(QBLogLevel.nothing)
+        
+        // Enables detailed XMPP logging in console output.
+        QBSettings.enableXMPPLogging()
+    }
+    
+    func connectToQuickBlox() {
+        
+        let currentUser = QBUUser()
+        currentUser.id = 30695887 // your current user's ID
+        currentUser.password = "Password1"; // your current user's password
+        
+        QBChat.instance().connect(with: currentUser) { (error) in
+            if let error = error {
+                print("Chat Server Connection Error: \(error.localizedDescription)")
+                return
+            }
+            
+            
+            // Connection Success
+            print("Connection Success")
+            QBRequest.logIn(withUserLogin: "sauvik_dolui", password: "Password1",
+                            successBlock: { (response, user) in
+                    print("Login Success")
+                    self.createADialoag(completionBlock: { (response, dialog) in
+                        if let _ = dialog {
+                            self.createADialoag(completionBlock: { (response, dialog) in
+                                //
+                            })
+                        } else {
+                            print("Error in creating a dialog")
+                            
+                        }
+                    })
+            }, errorBlock: { (response) in
+                print("Login Error = \(String(describing: response.error?.error?.localizedDescription))")
+            })
+            
+            
+        }
+    }
+    
+    func createADialoag(completionBlock: @escaping (QBResponse?, QBChatDialog?) -> Void) {
+        
+        let chatDialog = QBChatDialog(dialogID: nil, type: QBChatDialogType.group)
+        chatDialog.name = "Chat with Manish"
+        chatDialog.occupantIDs = [30695887, 30697285]
+        
+        QBRequest.createDialog(chatDialog, successBlock: { (response: QBResponse?, createdDialog : QBChatDialog?) -> Void in
+            if let createdDialog = createdDialog {
+                print("Created Dialog ID = \(String(describing: createdDialog.id))")
+            }
+            completionBlock(response, createdDialog)
+            
+        }) { (response : QBResponse!) -> Void in
+            completionBlock(response, nil)
+        }
+    }
 }
 
